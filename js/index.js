ead1c6d8a2b41b9f99eca448916dad1d3088374f
phucthinh291203 <110530334+phucthinh291203@users.noreply.github.com>
2022-08-15 13:14:51 +0700

    js

--- js/index.js
+++ js/index.js
@@ -15,7 +15,41 @@ function TaoThanhSearch(){
         document.getElementsByClassName('search-form').style.scaleY = 1;
 }
 
-let searchForm = document.querySelector('.search-form');
-document.querySelector('#search-btn').onclick = () =>{
-    searchForm.classList.toggle('active');
-}
+
+$(document).ready(function(){
+   $("#gotopbutton").hide()
+
+   $(window).scroll(function(){
+        if($(this).scrollTop() >= 100){
+                $("nav").css({
+                        "position":"fixed",
+                        "left":0,
+                        "right":0,
+                        "z-index":999,
+                        "top":0,
+                        "opacity":0.8,
+                })
+        }
+
+        else {
+                $("nav").css({
+                        "position":"relative",
+                        "z-index":999,
+                        "opacity":1,
+                })   
+        }
+   })
+
+   $(window).scroll(function(){
+        if($(this).scrollTop() >= 100)
+        $("#gotopbutton").show("slow")
+        else 
+        $("#gotopbutton").hide("slow")
+   })
+
+   $("#gotopbutton").click(function(){
+        $("html,body").animate({
+                scrollTop:0
+        },500);
+   })
+})
